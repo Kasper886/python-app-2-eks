95a0e39b1786c92ad0a75d064cb45f3b14ad202e
Alex Largman <51818001+Kasper886@users.noreply.github.com>
2021-11-08 20:48:27 +0200

    bash

---
+++ kubectl.sh
@@ -0,0 +1,15 @@
+#-----------------------#
+# Install Kube CTL      #
+#                       #
+# Created: 08.11.2021   # 
+# Owner: Alex Largman   # 
+#-----------------------#
+
+
+#!/bin/bash
+
+#Install Kube CTL
+curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.21.2/2021-07-05/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+kubectl version --short --client
