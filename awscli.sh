d0e2efe1c0134cbc46fc545abb492218ba0dc5f8
Alex Largman <51818001+Kasper886@users.noreply.github.com>
2021-11-08 20:45:54 +0200

    bash

---
+++ awscli.sh
@@ -0,0 +1,15 @@
+#------------------------------------#
+# Install AWS CLI, EKS CTL, Kube CTL #
+#                                    #
+# Created: 08.11.2021                # 
+# Owner: Alex Largman                # 
+#------------------------------------#
+
+
+#!/bin/bash
+#Install AWS CLI
+sudo apt install -y zip unzip
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+aws --version
