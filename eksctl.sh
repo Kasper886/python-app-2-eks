e3f7a294d70fb99ded208297923e09c5261dbd48
Alex Largman <51818001+Kasper886@users.noreply.github.com>
2021-11-08 20:49:51 +0200

    bash

--- eksctl.sh
+++ eksctl.sh
@@ -5,6 +5,7 @@
 # Owner: Alex Largman   # 
 #-----------------------#
 
+#!/bin/bash
 #Install EKS CTL
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /bin
